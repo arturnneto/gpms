9d31c9dbe43dda31adbd72d7f89d1049b5a6fd75
Artur Amorim <arturamorimdev@Gmail.com>
2025-07-14 20:58:49 -0300

    feat: add json converter to rabbit config

--- src/main/java/com/artur/gpms/config/mq/RabbitMqConfig.java
+++ src/main/java/com/artur/gpms/config/mq/RabbitMqConfig.java
@@ -1,9 +1,16 @@
 package com.artur.gpms.config.mq;
 
+import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class RabbitMqConfig {
 
     public static final String SALES_ORDER_QUEUE = "sale-order-created";
+
+    @Bean
+    public Jackson2JsonMessageConverter jackson2JsonMessageConverter() {
+        return new Jackson2JsonMessageConverter();
+    }
 }
