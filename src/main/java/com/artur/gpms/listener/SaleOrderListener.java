51cd5466c5a451edc45bf9f9a06d922effabeec4
Artur Amorim <arturamorimdev@Gmail.com>
2025-07-15 21:12:47 -0300

    feat: add sale order service on listener

--- src/main/java/com/artur/gpms/listener/SaleOrderListener.java
+++ src/main/java/com/artur/gpms/listener/SaleOrderListener.java
@@ -1,6 +1,7 @@
 package com.artur.gpms.listener;
 
 import com.artur.gpms.data.dtos.SaleOrderEvent;
+import com.artur.gpms.service.SaleOrderService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
@@ -13,9 +14,16 @@ import static com.artur.gpms.config.mq.RabbitMqConfig.SALES_ORDER_QUEUE;
 public class SaleOrderListener {
 
     private final Logger logger = LoggerFactory.getLogger(SaleOrderListener.class);
+    private final SaleOrderService saleOrderService;
+
+    public SaleOrderListener(SaleOrderService saleOrderService) {
+        this.saleOrderService = saleOrderService;
+    }
 
     @RabbitListener(queues = SALES_ORDER_QUEUE)
     public void listen(Message<SaleOrderEvent> message) {
         logger.info("Message consumed: {}", message);
+
+        saleOrderService.saveSaleOrderEvent(message.getPayload());
     }
 }
