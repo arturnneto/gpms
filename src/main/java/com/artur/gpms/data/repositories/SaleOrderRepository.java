bb4292bc5cef587d38880f86381520927eac1c74
Artur Amorim <arturamorimdev@Gmail.com>
2025-07-15 22:03:45 -0300

    feat: add find order by customer id method

--- src/main/java/com/artur/gpms/data/repositories/SaleOrderRepository.java
+++ src/main/java/com/artur/gpms/data/repositories/SaleOrderRepository.java
@@ -1,7 +1,10 @@
 package com.artur.gpms.data.repositories;
 
 import com.artur.gpms.data.entities.SaleOrderEntity;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
 public interface SaleOrderRepository extends MongoRepository<SaleOrderEntity, Long> {
+    Page<SaleOrderEntity> findAllByCustomerId(Long customerId, PageRequest pageRequest);
 }
