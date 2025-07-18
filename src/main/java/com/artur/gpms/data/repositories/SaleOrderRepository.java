ad34cbf40d4ec5c903640566bad71734feb10234
Artur Amorim <arturamorimdev@Gmail.com>
2025-07-18 00:28:55 -0300

    feat: add findbyorderid and findall repository methods

--- src/main/java/com/artur/gpms/data/repositories/SaleOrderRepository.java
+++ src/main/java/com/artur/gpms/data/repositories/SaleOrderRepository.java
@@ -2,9 +2,13 @@ package com.artur.gpms.data.repositories;
 
 import com.artur.gpms.data.entities.SaleOrderEntity;
 import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
+import java.util.Optional;
+
 public interface SaleOrderRepository extends MongoRepository<SaleOrderEntity, Long> {
-    Page<SaleOrderEntity> findAllByCustomerId(Long customerId, PageRequest pageRequest);
+    Page<SaleOrderEntity> findAllByCustomerId(Long customerId, Pageable pageable);
+    Optional<SaleOrderEntity> findByOrderId(String orderId);
+    Page<SaleOrderEntity> findAll(Pageable pageable);
 }
